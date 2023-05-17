e04ca2f045a5f250ba265f40addd63dfaba965a9
Temitope Oyeyinka <ttoyeyinka@gmail.com>
2023-05-17 04:05:40 +0100

    Completed lab 8

---
+++ lab8/part_c/application/model/createJson.php
@@ -0,0 +1,23 @@
+<?php
+echo '{ "users":[
+            {
+                "firstName":"Temitope",
+                "lastName":"Oyeyinka",
+                "joined": {
+                    "month":"November",
+                    "day":9,
+                    "year":1999
+                }
+            },
+            {
+                "firstName":"Lipe",
+                "lastName":"Akpeke",
+                "joined": {
+                    "month":"May",
+                    "day":12,
+                    "year":2000
+                }
+            }
+    ]}
+	';
+?>
